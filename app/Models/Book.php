14c44a5bd51b00dee41f011cecfcb48432c38b9f
Koehler <ntckoehler@gmail.com>
2023-01-25 14:48:26 -0400

    Update testes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $isbn
+ */
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'isbn'
+    ];
 }
--- tests/Feature/API/BooksControllerTest.php
+++ tests/Feature/API/BooksControllerTest.php
@@ -17,7 +17,7 @@ class BooksControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_books_get_endpoint()
+    public function test_get_books_endpoint()
     {
         $books = Book::factory(3)->create();
 
@@ -47,6 +47,63 @@ class BooksControllerTest extends TestCase
                 '0.title' => $book->title,
                 '0.isbn' => $book->isbn
             ]);
+        });
+    }
+
+    public function test_get_single_book_endpoint()
+    {
+        $book = Book::factory(1)->createOne();
+
+        $response = $this->getJson('/api/books/' . $book->id);
+
+        $response->assertStatus(200);
+
+        $response->assertJson(function (AssertableJson $json) use ($book)
+        {
+            $json->hasAll([
+                'id',
+                'title',
+                'isbn',
+                'created_at',
+                'updated_at'
+            ]);
+
+            $json->whereAllType([
+                'id' => 'integer',
+                'title' => 'string',
+                'isbn' => 'string'
+            ]);
+
+            $json->whereAll([
+                'id' => $book->id,
+                'title' => $book->title,
+                'isbn' => $book->isbn
+            ]);
+        });
+    }
+
+    public function test_post_book_endpoint()
+    {
+        $book = Book::factory(1)->makeOne()->toArray();
+
+        $response = $this->postJson('/api/books', $book);
+
+        $response->assertStatus(201);
+
+        $response->assertJson(function (AssertableJson $json) use ($book)
+        {
+            $json->hasAll([
+                'id',
+                'title',
+                'isbn',
+                'created_at',
+                'updated_at'
+            ]);
+
+            $json->whereAll([
+                'title' => $book['title'],
+                'isbn' => $book['isbn']
+            ])->etc();
         });
     }
 }
